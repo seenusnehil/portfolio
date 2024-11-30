2f3b795f8d9311733cbf2ff0fe5f0753b15db8a2
Snehil Seenu <snehilseenu@gmail.com>
2024-12-01 02:07:01 +0530

    new changes to the portfolio

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+function smoothScrollTo(target) {
+    const element = document.querySelector(target);
+    if (element) {
+        element.scrollIntoView({
+            behavior: 'smooth', // Enables smooth scrolling
+            block: 'start' // Scrolls to the top of the target section
+        });
+    }
+}
+
+
